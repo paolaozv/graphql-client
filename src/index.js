e7575c161b92e274ba293798d6c79b1891e5293a
Paola Ortiz de Zevallos Villanueva <paolaozv@gmail.com>
2018-06-07 00:04:58 -0500

    client apollo graphql

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import registerServiceWorker from './registerServiceWorker'
+import { ApolloClient } from 'apollo-client'
+import { HttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { ApolloProvider } from 'react-apollo' 
+
+const client = new ApolloClient({
+  link: new HttpLink({ uri: '/api' }),
+  cache: new InMemoryCache()
+})
+
+ReactDOM.render(
+<ApolloProvider client={client}>
+  <App />
+</ApolloProvider>, document.getElementById('root'))
+registerServiceWorker()
